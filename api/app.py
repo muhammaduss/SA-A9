3d65330b2eb5d8083fe5d8f4d6ead6a4e0c3c424
muhammaduss <muhammad20040130@gmail.com>
2024-12-06 23:01:56 +0300

    test results for brokers

--- api/app.py
+++ api/app.py
@@ -1,4 +1,7 @@
 import json
+import logging
+import sys
+import datetime
 from fastapi import FastAPI
 from schemas import ReceiveMessage
 from pika import BlockingConnection, ConnectionParameters
@@ -8,11 +11,19 @@ connection_parameters = ConnectionParameters(
     port=5672
 )
 
+logging.basicConfig(
+    level=logging.INFO,
+    stream=sys.stdout,
+    format="%(asctime)s | publisher | %(levelname)s | %(message)s",
+)
+
+
 app = FastAPI(title="User-Facing REST API Documentation")
 
 
 @app.post("/recieve")
 async def post_recieve_message(data: ReceiveMessage):
+    logging.info(f'Message recieved at: {datetime.datetime.now()}')
     with BlockingConnection(connection_parameters) as conn:
         with conn.channel() as ch:
             ch.queue_declare(queue='messages')
@@ -22,4 +33,4 @@ async def post_recieve_message(data: ReceiveMessage):
                 body=json.dumps(
                     {'user_alias': data.user_alias, 'message': data.message})
             )
-            print('Message sent to filter service')
+            logging.info('Message sent to filter service')
