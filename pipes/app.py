fb458260c88e4332b43162ae986ea169cc97b5c6
iliyasone <iliyas.dzabbarov@gmail.com>
2024-12-06 23:25:55 +0300

    pipes: working!

---
+++ pipes/app.py
@@ -0,0 +1,49 @@
+from contextlib import asynccontextmanager
+import logging
+import sys
+import json
+from fastapi import FastAPI
+from multiprocessing import Process
+
+from schemas import ReceiveMessage
+from filters import FilterFilter, Screamer, Publisher
+
+logging.basicConfig(
+    level=logging.INFO,
+    stream=sys.stdout,
+    format="%(asctime)s | pipe | %(levelname)s | %(message)s",
+)
+
+logging.info('start up')
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    filter_filter = FilterFilter()
+    screamer = Screamer()
+    publisher = Publisher()
+    filter_filter.pipe(screamer).pipe(publisher)
+
+    filter_process = Process(target=filter_filter.run, daemon=True)
+    screamer_process = Process(target=screamer.run, daemon=True)
+    publisher_process = Process(target=publisher.run, daemon=True)
+
+    filter_process.start()
+    screamer_process.start()
+    publisher_process.start()
+
+    app.state.pipeline_start = filter_filter
+    app.state.processes = [filter_process, screamer_process, publisher_process]
+
+    yield
+    app.state.pipeline_start.input_queue.put(None)
+    for p in app.state.processes:
+        p.join()
+
+
+app = FastAPI(title="User-Facing REST API Documentation", lifespan=lifespan)
+
+
+@app.post("/recieve")
+async def post_recieve_message(data: ReceiveMessage):
+    app.state.pipeline_start.input_queue.put(data.message)
+    return {"status": "message queued"}
--- pipes/filters.py
+++ pipes/filters.py
@@ -1,8 +1,24 @@
 from __future__ import annotations
+
 from dataclasses import dataclass, field
+
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+import json
 from multiprocessing import Process, Queue
 from abc import ABC, abstractmethod
+
+import os
+import smtplib
 import sys
+import logging
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 
 
 @dataclass
@@ -25,7 +41,7 @@ class Filter(ABC):
                 self.output_queue.put(None)
                 break
             transformed = self.apply(msg)
-            if transformed:
+            if transformed and self.output_queue is not None:
                 self.output_queue.put(transformed)
 
 
@@ -37,17 +53,48 @@ class FilterFilter(Filter):
             or "ailurophobia" in message
             or "mango" in message
         ):
-            print(
+            logging.info(
                 "FilterFilter info: Stop-word detected, "
                 + "message won't be send further\n"
             )
             return None
         return message
-    
+
+
 class Screamer(Filter):
     def apply(self, message):
         return message.upper()
-    
 
-class Publisher(Filter):
-    ...
+
+class Publisher(Filter): 
+    def apply(self, message):
+        from_m = os.getenv("FROM")
+        list_mails = os.getenv("LIST_MAILS")
+        password = os.getenv("PASS")
+        try:
+            email_addresses = json.loads(list_mails)
+        except json.JSONDecodeError as e:
+            logging.error(f"Error parsing LIST_MAILS: {e}")
+            return
+
+        try:
+            server = smtplib.SMTP("smtp.gmail.com", 587, timeout=10)
+            server.set_debuglevel(1)
+            server.starttls()
+            server.login(from_m, password)
+            for i in email_addresses: 
+                msg = MIMEMultipart()
+                msg["From"] = from_m
+                msg["To"] = i 
+                msg["Subject"] = "Testing"    
+                logging.info("From: %s, To: %s", from_m, i)
+                msg.attach(MIMEText(message, "plain"))
+                text = msg.as_string()
+                server.sendmail(from_m, i, text)
+                logging.info("Send email")
+            server.quit()
+        except Exception as e:
+            logging.info(f"Error {e}")
+
+        return message
+        
